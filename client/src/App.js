93a65a278cf8972c1e96e4ac3daa00545e68d91e
rbiagtan <rbiagtan@gmail.com>
2021-02-23 16:01:24 -0800

    initial sync

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React Feb 22 0626
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
