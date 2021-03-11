c98db5324d9bd084cc42274fb6893afbea90183a
rbiagtan <rbiagtan@gmail.com>
2021-03-10 21:35:29 -0800

    changed app js 2135

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React March 10 9:27 PM
+            Learn React
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
